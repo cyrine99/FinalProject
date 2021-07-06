d31a0d09f8cf356e3202d1b3cf5e29fe12b61755
cyrine99 <cyrineghwela1@gmail.com>
2021-07-07 00:38:03 +0200

    Add Comment

--- app/Http/Controllers/ParamedicsControllerAPI.php
+++ app/Http/Controllers/ParamedicsControllerAPI.php
@@ -7,56 +7,30 @@ use Illuminate\Http\Request;
 
 class ParamedicsControllerAPI extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Paramedics  $paramedics
-     * @return \Illuminate\Http\Response
-     */
     public function show(Paramedics $paramedics)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Paramedics  $paramedics
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Paramedics $paramedics)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Paramedics  $paramedics
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Paramedics $paramedics)
     {
         //
